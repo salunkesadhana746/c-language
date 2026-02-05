0100ab181e25ed527c1b96adae240e426f569e46
Sadhana Salunke <salunkesadhana746@gmail.com>
2026-02-05 10:53:37 +0530

    Add files via upload

---
+++ day 1/find2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	//2. WAP to Find area of sqare.
+	int a;
+	printf("Enter a :");
+	scanf("%d",&a);		
+	int area = a*a;
+	printf("Area of sqare :%d",area);
+	
+}
---
+++ day 1/formula2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	//2.WAP to find a the answer given formula (x-y)2.
+
+    int x,y;
+	printf("enter a value of x :");
+	scanf("%d",&x);
+	printf("enter a value of y :");
+	scanf("%d",&y);
+	
+	printf("(x-y)2 =%d",(x-y)*(x-y));
+}
