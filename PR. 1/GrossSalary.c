c640895ba3fcda96d323defa6dea248996f60761
Sadhana Salunke <salunkesadhana746@gmail.com>
2026-02-05 10:54:39 +0530

    Add files via upload

---
+++ PR. 1/GrossSalary.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float baseSalary,HRA,DA,TA,totalSalary;
+	
+	
+	printf("Enter base Salary :");
+	scanf("%f",&baseSalary);
+	
+	printf("Enter HRA% :");
+	scanf("%f",&HRA);
+	
+	printf("Enter DA% :");
+	scanf("%f",&DA);
+	
+	printf("Enter TA% :");
+	scanf("%f",&TA);
+	
+	float hraAmount = (HRA*baseSalary)/100;
+	float daAmount = (DA*baseSalary)/100;
+	float taAmount = (TA*baseSalary)/100;
+	
+	totalSalary = baseSalary + HRA + DA + TA;
+	printf("Total Salary : Rs. %.2f\n",totalSalary);
+	
+}
