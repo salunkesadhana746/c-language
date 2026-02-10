6655ba0a60267584d6e6dd6f90920dbfc7ae796c
Sadhana Salunke <salunkesadhana746@gmail.com>
2026-02-10 10:37:49 +0530

    Add files via upload

---
+++ day 2/formula3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	//3.WAP to find a the answer given formula (x+y)3.
+	int x,y;
+	printf("enter a value of x :");
+	scanf("%d",&x);
+	printf("enter a value of y :");
+	scanf("%d",&y);
+	
+	printf("(x+y)3 =%d",(x+y)*(x+y)*(x+y));
+}
---
+++ day 2/formula6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	//6.WAP to find a the answer given formula (x-y-z)3.
+	int x,y,z;
+	printf("enter a value of x :");
+	scanf("%d",&x);
+	printf("enter a value of y :");
+	scanf("%d",&y);
+	printf("enter a value of z :");
+	scanf("%d",&z);
+	
+	printf("(x-y-z)3 =%d",(x-y-z)*(x-y-z)*(x-y-z));
+}
