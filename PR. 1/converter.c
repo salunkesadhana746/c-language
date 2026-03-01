87c8edccd8a2612652a14460bcffe13b212f4790
Sadhana Salunke <salunkesadhana746@gmail.com>
2026-03-01 23:20:48 +0530

    Refactor temperature converter program structure

--- PR. 1/converter.c
+++ PR. 1/converter.c
@@ -2,8 +2,9 @@
 
 void main()
 {
-	//1. Temperature Converter
-	/*Write C program to convert temperature from degree Celsius to Fahrenheit using the formula: F=(9/5*C)+32*/
+	/*1. Temperature Converter
+	Write C program to convert temperature from degree Celsius to Fahrenheit using the formula: F=(9/5*C)+32*/
+	
 	int clesius;
 	 
 	printf("Enter  the temperature in Clesius :");
@@ -12,3 +13,4 @@ void main()
     float fahrenheit = (clesius*1.8 )+ 32;
 	printf("the temperature in fahrenheit: %.1f",fahrenheit);
 }
+
